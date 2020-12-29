cc77d41fa482b864ddd2fa065cecbf3b765bf4f9
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-12-29 23:06:45 +0900

    fix indent

--- pggan/tests/networks_test.py
+++ pggan/tests/networks_test.py
@@ -29,27 +29,28 @@ class GeneratorTestCase(unittest.TestCase):
             self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, N_CHANNEL, 2 ** res, 2 ** res])
             self.assertIsNone(getattr(self.generator.model, "fadein_module", None))
 
-    class DiscriminatorTestCase(unittest.TestCase):
-        def setUp(self) -> None:
-            self.discriminator = networks.Discriminator()
 
-        def test_init_model(self):
-            img = torch.ones(INPUT_VECTOR_SIZE, N_CHANNEL, 4, 4)
+class DiscriminatorTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.discriminator = networks.Discriminator()
+
+    def test_init_model(self):
+        img = torch.ones(INPUT_VECTOR_SIZE, N_CHANNEL, 4, 4)
+        outputs = self.discriminator(img)
+        self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, 1])
+        self.assertIsNone(getattr(self.discriminator.model, "fadein_module", None))
+
+    def test_grow_flush(self):
+        for res in [3, 4, 5, 6, 7, 8]:
+            img = torch.ones(INPUT_VECTOR_SIZE, N_CHANNEL, 2 ** res, 2 ** res)
+            self.discriminator.grow()
+            outputs = self.discriminator(img)
+            self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, 1])
+            self.assertIsNotNone(getattr(self.discriminator.model, "fadein_module", None))
+            self.discriminator.flush()
             outputs = self.discriminator(img)
             self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, 1])
             self.assertIsNone(getattr(self.discriminator.model, "fadein_module", None))
-
-        def test_grow_flush(self):
-            for res in [3, 4, 5, 6, 7, 8]:
-                img = torch.ones(INPUT_VECTOR_SIZE, N_CHANNEL, 2 ** res, 2 ** res)
-                self.discriminator.grow()
-                outputs = self.discriminator(img)
-                self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, 1])
-                self.assertIsNotNone(getattr(self.discriminator.model, "fadein_module", None))
-                self.discriminator.flush()
-                outputs = self.discriminator(img)
-                self.assertListEqual(list(outputs.size()), [INPUT_VECTOR_SIZE, 1])
-                self.assertIsNone(getattr(self.discriminator.model, "fadein_module", None))
 
 
 if __name__ == '__main__':
