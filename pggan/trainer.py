be94ebe9bd14ebd229566085e591dc791ebdd5c5
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-12-30 20:45:19 +0900

    give up list() due to out of memory

--- pggan/trainer.py
+++ pggan/trainer.py
@@ -5,7 +5,6 @@ import numpy as np
 import torch
 from torch import nn, optim
 from torchvision import transforms, utils as vutils
-from tqdm import tqdm
 
 from pggan import dataset, SnapshotDirectoryPath
 from pggan.config import Config
@@ -30,8 +29,7 @@ class Trainer:
         self.level_updated()
 
     def level_updated(self):
-        print("Loading data")
-        self.dataloader = [b for b in tqdm(dataset.dataloader(self.resolution))]
+        self.dataloader = dataset.dataloader(self.resolution)
         self.lr = Config.LEARNING_RATE * Config.LEARNING_RATE_DECAY ** (self.resolution - Config.MIN_RESOLUTION)
         self.opt_g = optim.Adam(self.generator.parameters(), lr=self.lr, betas=(Config.BETA1, Config.BETA2))
         self.opt_d = optim.Adam(self.discriminator.parameters(), lr=self.lr, betas=(Config.BETA1, Config.BETA2))
